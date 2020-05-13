0f43787686c5edc0fbf8cc328342bb96a276de71
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-05-13 14:06:55 +0530

    adding animation

--- Graph Animation/Simple_1D_walk.py
+++ Graph Animation/Simple_1D_walk.py
@@ -36,3 +36,43 @@ X, epsilon = random_walk(N)
 
 # normalizing the random walk using the Central Limit Theoram
 X = X*np.sqrt(1./N)
+
+
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+fig = plt.figure(figsize= (20, 10))
+ax = plt.axes(xlim= (0, N), ylim= (np.min(X)-0.5, np.max(X)+0.5))
+line, = ax.plot([], [], lw= 2, color= 'red')
+ax.set_xticks(np.arange(0, N+1, 50))
+ax.set_yticks(np.arange(np.min(X)-0.5, np.max(X)+0.5, 0.2))
+ax.set_title("Simple 1D random walk", fontsize= 22)
+ax.set_xlabel('Steps', fontsize= 18)
+ax.set_ylabel('Value', fontsize= 18)
+ax.tick_params(labelsize= 16)
+ax.grid(True, which= 'major', linestyle= '--', color= 'black', alpha= 0.4)
+
+
+# initialization function
+def init():
+    # creating empty plot/frame
+    line.set_data([],[])
+    return line,
+
+# List to store x and y points
+xdata, ydata = [], []
+
+# animation function
+
+def animate(i):
+    y = X[i]
+    xdata.append(i)
+    ydata.append(y)
+    line.set_data(xdata, ydata)
+    return line,
+
+# call the animator
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames= N, interval =20, blit= True)
+# anim.save('random_walk.gif')  *error : movie writter not available
